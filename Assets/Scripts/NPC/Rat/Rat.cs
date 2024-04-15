9fe2a9d50454ee555d2d0ba247393c44bc56f13a
LetsBigSnack <83255386+LetsBigSnack@users.noreply.github.com>
2024-04-15 09:03:20 +0200

    Added Rat Rotation and Spawning Particle Prefab

--- Assets/Scripts/NPC/Rat/Rat.cs
+++ Assets/Scripts/NPC/Rat/Rat.cs
@@ -12,13 +12,16 @@ public class Rat : MonoBehaviour
     [SerializeField] private bool _isBeingSacrificed = false;
     [SerializeField] private float sacrificeTime = 2.0f;
     
-
+    public float speed = 30f; // Speed of rotation
+    public float moveSpeed = 2f; // Speed of movement
+    public float levitationHeight = 1f; // Height of levitation
 
     public void StartSacrifice()
     {
         if (!_isBeingSacrificed)
         {
             _isBeingSacrificed = true;
+            StartCoroutine(MoveAndRotateCoroutine());
             StartCoroutine(GetSacrificed()); 
         }
     }
@@ -34,7 +37,7 @@ public class Rat : MonoBehaviour
         //Spawn Blood
         
         Vector3 pos = transform.position;
-        Quaternion rotation = transform.rotation;
+        Quaternion rotation = Quaternion.identity;
 
         Instantiate(prefabBlood, pos, rotation);
         
@@ -42,4 +45,25 @@ public class Rat : MonoBehaviour
         Destroy(gameObject);
     }
     
+    public IEnumerator MoveAndRotateCoroutine()
+    {
+        Vector3 startHeight = transform.position;
+        Vector3 endHeight = new Vector3(transform.position.x, transform.position.y + levitationHeight, transform.position.z);
+        float journey = 0f;
+
+        // Move the rat vertically upwards to the maximum height
+        while (journey <= 1f)
+        {
+            journey += Time.deltaTime * moveSpeed;
+            transform.position = Vector3.Lerp(startHeight, endHeight, journey);
+            yield return null;
+        }
+
+        // Begin spinning once the maximum height is reached
+        while (_isBeingSacrificed)
+        {
+            transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
 }
